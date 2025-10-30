b12c0cf09c78e71761e6b348bdce465cbbe112ca
Andrea <andrea.laguillo@externo.cnic.es>
2025-10-30 16:37:27 +0100

    add bat file
    installs requirements if needed and runs refmod

---
+++ RefMod.bat
@@ -0,0 +1,46 @@
+
+@echo off
+setlocal
+
+echo Attempting to launch RefMod...
+python src\RefMod_GUI.py
+if %errorlevel% equ 0 (
+    goto :eof
+)
+
+REM echo Launch failed. Checking Python and pip...
+REM python -m ensurepip >nul 2>&1
+REM if %errorlevel% neq 0 (
+REM     echo Python or pip is not installed. Please install Python first.
+REM     pause
+REM     exit /b
+REM )
+
+echo Launch failed. Checking Python installation...
+where python >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Python is not installed or not in PATH. Please install Python.
+    pause
+    exit /b
+)
+
+echo Checking pip installation...
+python -m pip --version >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Pip not found. Attempting to install pip...
+    python -m ensurepip
+)
+
+
+echo Installing requirements...
+pip install -r requirements.txt
+
+echo Trying to launch RefMod again...
+python src\RefMod_GUI.py
+if %errorlevel% equ 0 (
+    goto :eof
+)
+
+echo Failed to launch RefMod.
+pause
+exit /b
